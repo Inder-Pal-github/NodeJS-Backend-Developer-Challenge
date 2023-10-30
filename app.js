43e64b69d7a157b6c5f4b1e1fde69c024cc67c71
Inder-Pal-github <inder39811@gmail.com>
2023-10-30 17:39:34 +0530

    UPDATE: add configuration for swagger docs

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const dbConnection = require("./config/dbConnection"); // Database Connection
 const bookRouter = require("./routes/book.routes");
 require("dotenv").config();
 
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+  openapi:"3.0.0",
+  info:{
+    title:"RESTful API using Node.js for managing books.",
+    version:"1.0.0",
+  }
+}
+const options = {
+  swaggerDefinition,
+  apis:["./routes/*routes.js"]
+}
+
+const swaggerSpec = swaggerJSDoc(options);
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -10,6 +27,8 @@ const PORT = process.env.PORT || 8080;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// swagger documentation
+app.use("/docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
 // book router
 app.use("/api/v1", bookRouter);
