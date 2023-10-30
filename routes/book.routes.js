cf5d9e170216a11d114a1a3669b5ac61967b4237
Inder-Pal-github <inder39811@gmail.com>
2023-10-30 13:41:19 +0530

    UPDATE: book routes

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,8 +1,18 @@
-const {Router} = require("express");
-
+const { Router } = require("express");
+const {
+  getBooks,
+  createBook,
+  getBookById,
+  updateBook,
+  deleteBook,
+} = require("../controllers/books.controller");
 
 const bookRouter = Router();
 
-bookRouter.get("/books",)
+bookRouter.post("/book/new", createBook);
+bookRouter.get("/books", getBooks);
+bookRouter.get("/book/:id", getBookById);
+bookRouter.patch("/book/:id", updateBook);
+bookRouter.delete("/book/:id", deleteBook);
 
 module.exports = bookRouter;
